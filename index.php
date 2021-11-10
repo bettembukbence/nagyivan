a3c435869f4514d4243b9735eb4c48ed5d12b88b
Bettembuk Bence <bettembukbence@gmail.com>
2021-11-10 15:12:17 +0100

    index.php megÍrása

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'protected/utils/definitions.php';
+require_once 'protected/html_elements/header.php';
+?>
+
+<body>
+
+<?php  require_once 'protected/html_elements/nav.php'; ?>
+
+<main role="main" class="container">
+
+    <div class="starter-template">
+        <?php  require_once 'protected/pages/index.php'; ?>
+    </div>
+
+</main>
+
+<?php  require_once 'protected/html_elements/footer.php'; ?>
+
+
